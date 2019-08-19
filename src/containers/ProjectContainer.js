127e4a0b525a340a2cd15f05701ad2fcd71fe9e2
Richard <richardli125@gmail.com>
2019-08-19 00:06:24 -0400

    Added create project div

--- src/containers/ProjectContainer.js
+++ src/containers/ProjectContainer.js
@@ -1,13 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Project from "../components/Project";
 
 class ProjectContainer extends Component {
+  state = {
+    projectList: [
+      { name: 'Project1'},
+      { name: 'Project2'},
+      { name: 'Project3'}
+    ]
+  };
+
+  handleClick = () => {
+    this.setState({
+      projectList: [{ name: "New Project" }, ...this.state.projectList]
+    });
+    // console.log('hello')
+  };
+
   render() {
     return (
-      <div className="project-container">
-        <h2>Project Container</h2>
+      <div className='project-container'>
+        <div>
+          <h2>All Projects</h2>
+        </div>
+          <Project projects={this.state.projectList} handleClick={this.handleClick} />
       </div>
-    )
+    );
   }
 }
 
-export default ProjectContainer
+export default ProjectContainer;
