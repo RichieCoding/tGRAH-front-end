0f931cfd89d6b74bfec36b2c9331313ec01a2c85
Richard Li <richardli125@gmail.com>
2019-08-19 10:43:57 -0400

    added react-router-dom save

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import About from './components/About'
-import Header from './components/Header';
-import Sidebar from './containers/Sidebar'
-import ProjectContainer from './containers/ProjectContainer'
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import About from "./components/About";
+import Header from "./components/Header";
+import Sidebar from "./containers/Sidebar";
+import ProjectContainer from "./containers/ProjectContainer";
 
 function App() {
   return (
     <Router>
-        <Header />
+      <Header />
       <Switch>
-        <Route path="/about" component={About} />
-      <div className="home-container">
-        <Sidebar />
-        <ProjectContainer />
-      </div>
+        <Route path='/about' component={About} />
+        <div className='home-container'>
+          <Sidebar />
+          <ProjectContainer />
+        </div>
       </Switch>
     </Router>
   );
 }
 
 export default App;
-
