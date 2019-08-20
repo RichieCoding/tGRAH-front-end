ae344806ef28d08553d7bdb244ad57ab14b98821
Richard Li <richardli125@gmail.com>
2019-08-20 16:33:24 -0400

    Finished login function

--- src/App.js
+++ src/App.js
@@ -7,27 +7,109 @@ import Sidebar from "./containers/Sidebar";
 import ProjectContainer from "./containers/ProjectContainer";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import ListContainer from "./containers/ListContainer"
+import ListContainer from "./containers/ListContainer";
 
 class App extends Component {
-
   state = {
-    login: true
+    login: false
+  };
+
+  componentDidMount() {
+    // Check local storage for a token
+    this.checkForToken();
   }
 
+  checkForToken = () => {
+    localStorage.token
+      ? this.getUserFromToken()
+      : console.log("You're not logged in, buddy!!");
+  };
+
+  // Get user from token
+  getUserFromToken = () => {
+    fetch("http://localhost:3000/persist", {
+      method: "GET",
+      headers: {
+        Authorization: localStorage.token,
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      }
+    })
+      .then(res => res.json())
+      .then(userInfo => {
+        this.setState(
+          {
+            login: true,
+            currentUser: userInfo
+          },
+          () => {
+            console.log(this.state.currentUser);
+          }
+        );
+      });
+  };
+
+  logInUser = (username, password) => {
+    fetch("http://localhost:3000/tokens", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({ username, password })
+    })
+      .then(res => res.json())
+      .then(userInfo => {
+        this.setState(
+          {
+            login: true,
+            currentUser: userInfo
+          },
+          () => {
+            // store token in local storage
+            localStorage.setItem("token", userInfo.token);
+          }
+        );
+      });
+  };
+
   render() {
-    return (
+    return this.state.login ? (
       <Router>
         <Header login={this.state.login} />
         <Switch>
-          <Route path='/about' component={About} />
-          <Route path='/login' component={Login} />
-          <Route path='/signup' component={Signup} />
-          <Route path='/project' component={ListContainer} />
-          <div className='home-container'>
-            <Sidebar />
-            <ProjectContainer />
-          </div>
+          <Route exact path='/about' component={About} />
+          <Route exact path='/project' component={ListContainer} />
+          <Route
+            exact
+            path='/'
+            render={routerProps => (
+              <div className='home-container'>
+                <Sidebar />
+                <ProjectContainer />
+              </div>
+            )}
+          />
+        </Switch>
+      </Router>
+    ) : (
+      <Router>
+        <Header login={this.state.login} />
+        <Switch>
+          <Route
+            exact
+            path='/login'
+            render={routerProps => (
+              <Login {...routerProps} logInUser={this.logInUser} />
+            )}
+          />
+          <Route exact path='/signup' component={Signup} />
+          <Route
+            path='/*'
+            render={routerProps => (
+              <Login {...routerProps} logInUser={this.logInUser} />
+            )}
+          />
         </Switch>
       </Router>
     );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 class Login extends Component {
   state = {
@@ -13,31 +13,45 @@ class Login extends Component {
     });
   };
 
+  handleSubmit = e => {
+    e.preventDefault()
+    let { username, password } = this.state;
+    this.props.logInUser(username, password)
+  };
+
   render() {
     return (
       <div className='login-component'>
-        <div id="login-header">
+        <div id='login-header'>
           <h2>Login</h2>
         </div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input
             type='text'
             name='username'
             placeholder='Username'
             onChange={this.handleChange}
             value={this.state.username}
-          /> <br></br>
+          />{" "}
+          <br />
           <input
             type='password'
             name='password'
             placeholder='Password'
             onChange={this.handleChange}
             value={this.state.password}
-          /> <br></br>
+          />{" "}
+          <br />
           <input type='submit' />
         </form>
-        <p><Link to={'/signup'}>Create an Account</Link></p>
-        <img id="puglove" src='https://media0.giphy.com/media/onsxwAbM8xUNa/source.gif' alt="doggie"/>
+        <p>
+          <Link to={"/signup"}>Create an Account</Link>
+        </p>
+        <img
+          id='puglove'
+          src='https://media0.giphy.com/media/onsxwAbM8xUNa/source.gif'
+          alt='doggie'
+        />
       </div>
     );
   }
